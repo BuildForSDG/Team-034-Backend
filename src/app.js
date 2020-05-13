1e082f4428fd80818e86e0bfbe727c57672179f0
Oluwaseun Michael Oluwatomisin <62293658+ooluwaseunmicheal@users.noreply.github.com>
2020-05-13 10:52:47 +0100

    Revert "#1 Implements signup endpoints"

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import router from './routes/routes';
-import cors from 'cors';
+const app = async () => '#BuildforSDG';
 
-const app = express();
-dotenv.config();
-
-app.get('/', (req, res) => {
-  res.send('Welcome! SaferRoad App');
-});
-
-app.use(cors());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/api/v1/', router);
-
-const { PORT } = process.env;
-
-app.listen(PORT, () => console.log(`Server running on port ${ PORT }`));
+export default app;
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import signup from '../controllers/register';
-
-const router = express.Router();
-
-router.post('/auth/signup', signup);
-
-export default router;
-
